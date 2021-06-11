f00b3e17953debd1c2017b088373c6bb211a9d37
jjh9000507 <jjh9000507@hanmail.net>
2021-06-12 06:01:37 +0900

    6/12 python study

---
+++ string_operator02.py
@@ -0,0 +1,16 @@
+print("문자를 뒤에서 선택해 볼까요?")
+print("안녕하세요"[-1])
+print("안녕하세요"[-2])
+print("안녕하세요"[-3])
+print("안녕하세요"[-4])
+print("안녕하세요"[-5])
+
+print("안녕하세요" * 3)
+print("안녕하세요"[1:4])
+print("안녕하세요"[1:])
+print("안녕하세요"[:3])
+
+hello = "안녕하세요"
+print(hello[0:2])
+print(hello)
+print(len("안녕하세요"))
